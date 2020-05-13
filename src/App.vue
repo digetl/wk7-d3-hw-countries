8df160f58677949dafde25410d8cd2c2f6338e54
Dave Campbell <dave@digetl.co.uk>
2020-05-13 19:41:30 +0100

    start of homework

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <h1>Country App</h1>
+    <countries-list :countries='countries'></countries-list>
+    <country-detail :country='selectedCountry'></country-detail>
+  </div>
+</template>
+
+<script>
+import { eventBus } from "./main.js";
+import CountriesList from "./components/CountriesList.vue";
+import CountryDetail from "./components/CountryDetail.vue";
+
+export default {
+  name: "app",
+  data() {
+    return {
+      countries: [],
+      selectedCountry: null
+    };
+  },
+  mounted() {
+    fetch("https://restcountries.eu/rest/v2/all")
+      .then(result => result.json())
+      .then(countries => (this.countries = countries));
+
+    eventBus.$on("coutry-selected", country => {
+      this.selectedCountry = country
+    });
+  },
+
+  components: {
+    "countries-list": CountriesList,
+    "country-detail": CountryDetail
+  }
+};
+</script>
+
+<style>
+</style>
